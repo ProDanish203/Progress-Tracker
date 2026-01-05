22d336f3c65e158c7d35a5e3a8063c2d53960d00
Danish <danishsidd524@gmail.com>
2026-01-06 01:52:04 +0500

    init

---
+++ lib/data.ts
@@ -0,0 +1,15 @@
+export const navLinks = [
+  {
+    label: "Sticky Notes",
+    href: "/",
+  },
+  {
+    label: "Goals",
+    href: "/goals",
+  },
+];
+
+export const metaTitle = "Progress Tracker";
+export const metaDescription =
+  "A Simple and efficient progress tracking application with custom tracking of tasks, goals, and todos, encouraging the person to manage all their goals into one place, stored locally, safe and secure, with a bit of gamification included.";
+export const siteUrl = "https://progress-tracker-ds.vercel.app"
